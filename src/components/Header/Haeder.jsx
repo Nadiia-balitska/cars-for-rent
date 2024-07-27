f58f7043f08139c9e0f6b36b4a7468e13148a7f9
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2024-07-27 04:46:44 +0200

    add homepage

--- src/components/Header/Haeder.jsx
+++ src/components/Header/Haeder.jsx
@@ -2,8 +2,62 @@ import { NavLink } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <>
-      <header>
+    <header className="absolute inset-x-0 top-0 z-50">
+      <nav
+        aria-label="Global"
+        className="flex items-center justify-between p-6 lg:px-8"
+      >
+        <div className="flex lg:flex-1">
+          <h2 className=" font-bold text-lg leading-8text-gray-900">
+            Car rent
+          </h2>
+          {/* <img
+            alt=""
+            src="https://img.freepik.com/premium-vector/car-rental-logo-vector_221979-136.jpg?w=100"
+            className="h-8 w-auto"
+          /> */}
+        </div>
+
+        <div className="hidden lg:flex lg:gap-x-12">
+          <NavLink className="btn btn-outline btn-active " to="/catalog">
+            Catalog
+          </NavLink>
+
+          <NavLink className="btn btn-outline btn-secondary" to="/favorites">
+            Favorites
+          </NavLink>
+        </div>
+      </nav>
+    </header>
+  );
+};
+//  <div className="flex lg:flex-1">
+//     <h2 className="sr-only">Car Rental</h2>
+//     {/* <img
+//       className="h-8 w-auto"
+//       src="https://img.freepik.com/premium-vector/car-rental-logo-vector_221979-136.jpg?w=2000"
+//       alt=""
+//     /> */}
+//   </div>
+
+//   <div className="hidden lg:flex lg:gap-x-12">
+// <NavLink
+//   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+//   to="/catalog"
+// >
+//   Catalog
+// </NavLink>
+
+// <NavLink
+//   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+//   to="/favorites"
+// >
+//   Favorites
+// </NavLink>
+// </div>
+
+{
+  /* <header>
         <h2>Car Rental</h2>
         <ul>
           <li>
@@ -14,7 +68,5 @@ export const Header = () => {
             <NavLink to="/favorites">Favorites</NavLink>
           </li>
         </ul>
-      </header>
-    </>
-  );
-};
+      </header> */
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,58 @@
+// import { Link } from "react-router-dom";
+// import s from "./Header.modules.css";
+
+import { Link } from "react-router-dom";
+// import { Header } from "../../components/Header/Haeder";
+
+export const Home = () => {
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://media.licdn.com/dms/image/C561BAQEffhkajkAH8w/company-background_10000/0/1584307203232/jesses_garage_european_auto_repair_cover?e=2147483647&v=beta&t=lIWtpLB0w_skxRZn5YwfE6ujHpAxJ1K3un-5dxkajcg)",
+      }}
+    >
+      <div className="hero-overlay p-11">
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md ">
+            <h1 className="text-5xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Welcome to us!
+            </h1>
+            <p className="py-4 text-lg leading-8 text-gray-600">
+              We will help you to choose any type of car for rent, please FOLLOW
+              us bellow.
+            </p>
+            <div className=" flex items-center justify-center gap-x-6">
+              <Link to="/catalog" className="btn btn-active btn-secondary">
+                Get Started
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+/* // <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://th.bing.com/th/id/OIP.QFXh3tAyzlYvUEO_IGhXZgHaE7?rs=1&pid=ImgDetMain)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5 font-bold">
+            We will help you to choose any type of car for rent, please FOLLOW
+            us bellow 🕵🏻‍♀️
+          </p>
+          <Link to="/catalog" className="btn btn-neutral">
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </div> */
